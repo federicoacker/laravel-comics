7db2304bb5e895a3101457c0804ba3a45ead46b8
federicoacker <acker.federico@gmail.com>
2026-09-14 15:35:52 +0200

    Completato l'header

--- resources/views/partials/header.blade.php
+++ resources/views/partials/header.blade.php
@@ -1,43 +1,55 @@
 <header class="main-header">
-    <nav class="navbar navbar-expand-lg bg-body-tertiary">
-        <div class="container-fluid">
-            <a class="navbar-brand" href="#">Navbar</a>
+    @php
+        $page_name = basename($_SERVER['PHP_SELF']);
+    @endphp
+    <div class="container h-100">
+        <nav class="navbar navbar-expand-lg bg-body-tertiary align-items-stretch h-100">
+            <a class="navbar-brand" href="{{ route("homepage") }}">
+                <img class="img-fluid" src="{{ Vite::asset("resources/img/dc-logo.png") }}" />
+            </a>
+            <div class="collapse navbar-collapse align-items-stretch justify-content-end h-100"
+                id="navbarSupportedContent">
+                <ul class="navbar-nav mb-2 mb-lg-0 justify-content-end h-100">
+                    <li class="nav-item">
+                        <a class="{{$page_name == "characters" ? "nav-link active" : "nav-link"}}"
+                            href="#">Characters</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="{{$page_name == "index.php" ? "nav-link active" : "nav-link"}}" aria-current="page"
+                            href="{{ route("homepage") }}">Comics</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="{{$page_name == "movies" ? "nav-link active" : "nav-link"}}" href="#">Movies</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="{{$page_name == "tv" ? "nav-link active" : "nav-link"}}" href="#">Tv</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="{{$page_name == "games" ? "nav-link active" : "nav-link"}}" href="#">Games</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="{{$page_name == "collectibles" ? "nav-link active" : "nav-link"}}"
+                            href="#">Collectibles</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="{{$page_name == "videos" ? "nav-link active" : "nav-link"}}" href="#">Videos</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="{{$page_name == "fans" ? "nav-link active" : "nav-link"}}" href="#">Fans</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="{{$page_name == "news" ? "nav-link active" : "nav-link"}}" href="#">News</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="{{$page_name == "shop" ? "nav-link active" : "nav-link"}}" href="#">Shop</a>
+                    </li>
+                </ul>
+            </div>
             <button class="navbar-toggler" type="button" data-bs-toggle="collapse"
                 data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false"
                 aria-label="Toggle navigation">
                 <span class="navbar-toggler-icon"></span>
             </button>
-            <div class="collapse navbar-collapse" id="navbarSupportedContent">
-                <ul class="navbar-nav me-auto mb-2 mb-lg-0">
-                    <li class="nav-item">
-                        <a class="nav-link active" aria-current="page" href="#">Home</a>
-                    </li>
-                    <li class="nav-item">
-                        <a class="nav-link" href="#">Link</a>
-                    </li>
-                    <li class="nav-item dropdown">
-                        <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown"
-                            aria-expanded="false">
-                            Dropdown
-                        </a>
-                        <ul class="dropdown-menu">
-                            <li><a class="dropdown-item" href="#">Action</a></li>
-                            <li><a class="dropdown-item" href="#">Another action</a></li>
-                            <li>
-                                <hr class="dropdown-divider">
-                            </li>
-                            <li><a class="dropdown-item" href="#">Something else here</a></li>
-                        </ul>
-                    </li>
-                    <li class="nav-item">
-                        <a class="nav-link disabled" aria-disabled="true">Disabled</a>
-                    </li>
-                </ul>
-                <form class="d-flex" role="search">
-                    <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search" />
-                    <button class="btn btn-outline-success" type="submit">Search</button>
-                </form>
-            </div>
-        </div>
-    </nav>
+        </nav>
+    </div>
 </header>
--- routes/web.php
+++ routes/web.php
@@ -5,3 +5,7 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('home');
 })->name("homepage");
+
+Route::get("/test", function () {
+    return view("test");
+});
